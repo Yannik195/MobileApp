60d859a6bacf818e494af9b36274f38e60485f19
Yannik195 <yannisim@gmail.com>
2019-11-03 18:00:48 +0100

    Toolbar & Card

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,35 @@
 <template>
-  <v-app>
-    <v-app-bar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        text
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
-    </v-app-bar>
+  <v-app class="content">
+    <AppBar />
 
     <v-content>
-      <HelloWorld/>
+      <LocationCard />
+      <HelloWorld />
+      <HelloWorld />
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import HelloWorld from "./components/HelloWorld";
+import AppBar from "./components/AppBar.vue";
+import LocationCard from "./components/LocationCard.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     HelloWorld,
+    AppBar,
+    LocationCard
   },
   data: () => ({
     //
-  }),
+  })
 };
 </script>
+
+<style>
+.content {
+  margin: 12px;
+}
+</style>
---
+++ src/components/AppBar.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <v-app-bar class="app-bar" hide-on-scroll fixed app color="primary" dark flat>
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      <v-toolbar-title>My Mobile App</v-toolbar-title>
+      <v-spacer></v-spacer>
+
+      <v-tooltip bottom>
+        <template v-slot:activator="{ on }">
+          <v-btn v-on="on" icon>
+            <v-icon>mdi-magnify</v-icon>
+          </v-btn>
+        </template>
+        <span>Search</span>
+      </v-tooltip>
+
+      <v-tooltip bottom>
+        <template v-slot:activator="{on}">
+          <v-btn v-on="on" icon>
+            <v-icon>mdi-heart</v-icon>
+          </v-btn>
+        </template>
+        <span>Heart</span>
+      </v-tooltip>
+
+      <v-tooltip bottom>
+        <template v-slot:activator="{on}">
+          <v-btn v-on="on" icon>
+            <v-icon>mdi-dots-vertical</v-icon>
+          </v-btn>
+        </template>
+        <span>More</span>
+      </v-tooltip>
+
+    </v-app-bar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AppBar"
+};
+</script>
+
+<style scoped>
+</style>
